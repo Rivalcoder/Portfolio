8b922ee98469d22e490ed6c1c24943c4004dbe1d
Rivalcoder <divi242006@gmail.com>
2025-01-21 21:31:40 +0530

    [Edit] In Bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { FaInstagram, FaGithub, FaLinkedin, FaTelegram, FaJava } from "react-icons/fa";
+import { FaInstagram, FaGithub, FaLinkedin, FaTelegram, FaJava ,FaTelegramPlane} from "react-icons/fa";
 import image from "../assets/photo.jpg";
 import image2 from "../assets/analyser.png";
 import image3 from "../assets/messenger.png";
@@ -7,6 +7,7 @@ import { useState } from "react";
 import Procard from "../components/procard";
 import { BiLogoFirebase } from "react-icons/bi";
 import { MdOutlineMenuOpen } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 import { SiC, SiExpress, SiPython, SiJavascript, SiMongodb, SiFigma, SiReact, SiNextdotjs, SiSocketdotio, SiFlask } from "react-icons/si";
 
 export default function Home() {
@@ -25,10 +26,10 @@ export default function Home() {
                     <a href="/" className="hover-text-3xl ml-3 hover:scale-110 text-transparent bg-clip-text bg-gradient-to-r from-orange-400 via-yellow-300 to-green-400 hover:text-red-600 transition-all duration-200 ">Rival Coder</a>
                 </div>
                     <ul className={`flex flex-row gap-10 text-xl mr-4 max-sm:flex-col ${click?"max-sm:hidden":"max-sm:flex"} max-sm:p-5 max-sm:self-start max-sm:bg-[#060335] max-sm:w-full `}>
-                        <li className="hover:scale-110 transition-all duration-200 "><a href="#" className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5">Home</a></li>
-                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Skills" className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 ">Skills</a></li>
-                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Projects" className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5">Projects</a></li>
-                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Contact" className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 ">Contact</a></li>
+                        <li className="hover:scale-110 transition-all duration-200 "><a href="#"  onClick={checker} className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 duration-200">Home</a></li>
+                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Skills" onClick={checker} className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 duration-200">Skills</a></li>
+                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Projects" onClick={checker} className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 duration-200">Projects</a></li>
+                        <li className="hover:scale-110 transition-all duration-200 "><a href="#Contact" onClick={checker} className="hover:border-b-[3px] p-2 transition-all rounded-sm max-sm:p-5 duration-200 ">Contact</a></li>
                     </ul>
                 </nav>
             </header>
@@ -46,7 +47,7 @@ export default function Home() {
                             </ul>
                         </div>
                     </div>
-                    <div className="flex-1">
+                    <div className="flex-1 max-sm:hidden">
                         <div className="h-1/2 w-full">
                             <img src={image} alt="No Image" className="rounded-3xl"></img>
                         </div>
@@ -54,9 +55,9 @@ export default function Home() {
                 </section>
             </section>
             <label id="Skills"></label>
-            <section className="flex flex-col justify-center items-center min-h-screen">
-                <h1 className="pt-24 font-semibold text-5xl flex justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-orange-400 to-red-500">My Skills</h1>
-                <div className="grid grid-cols-5 mt-16 max-sm:grid-cols-2">
+            <section className=" flex flex-col justify-center items-center min-h-[80vh]">
+                <h1 className="pt-24  font-semibold text-5xl flex justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-orange-400 to-red-500">My Skills</h1>
+                <div className="grid grid-cols-5 mt-16 max-sm:grid-cols-2 max-lg:grid-cols-3">
                     <Skills img={<SiExpress className="text-yellow-700 text-6xl hover:text-yellow-600 transition-all duration-200" />} head="Express" message="" />
                     <Skills img={<SiMongodb className="text-green-500 text-6xl hover:text-green-600 transition-all duration-200" />} head="MongoDb" message="" />
                     <Skills img={<SiJavascript className="text-yellow-500 text-6xl hover:text-yellow-600 transition-all duration-200" />} head="JavaScript" message="" />
@@ -72,27 +73,59 @@ export default function Home() {
             <label id="Projects"></label>
             <section className="pt-24 flex flex-col justify-center items-center">
                 <h1 className="pb-10 mb-10 font-semibold text-5xl flex justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-gray-200 via-orange-400 to-red-500">My Projects</h1>
-                <div className="grid grid-cols-2 gap-9 w-[70%] max-sm:grid-cols-1">
+                <div className="grid grid-cols-2 gap-9 w-[70%] max-lg:grid-cols-1 max-lg:w-[80%]">
                     <Procard name="Resume Analyser(ATS)" img={image2} data="Analyse resumes instantly with AI-powered insights, optimizing job applications for success." />
                     <Procard name="Mini-Messenger" img={image3} data="Chat securely with Mini-Messenger, a real-time app using username and room ID for private conversations" />
-                    <Procard name="Mini-Messenger" img={image3} data="Chat securely with Mini-Messenger, a real-time app using username and room ID for private conversations" />
-                    <Procard name="Mini-Messenger" img={image3} data="Chat securely with Mini-Messenger, a real-time app using username and room ID for private conversations" />
-                </div>
+                    </div>
             </section>
             <label id="Contact"></label>
             <section className="pt-24 flex flex-col justify-center items-center pb-10 lg:mt-5">
                 <footer className="flex flex-col gap-10 justify-center items-center">
                     <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-orange-400 via-yellow-300 to-green-400 transition-all duration-200">Rivalcoder</h1>
-                    <div className="flex gap-10 max-sm:flex-col ">
-                        <a className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200" href="https://github.com/Rivalcoder" target="_blank">GitHub</a>
-                        <a className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200" href="https://www.instagram.com/rival_coder?utm_source=qr&igshid=YnQ3dWxwdXFvMWNi" target="_blank">Instagram</a>
-                        <a className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200" href="https://www.linkedin.com/in/divyaprakash-r-349746292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">LinkedIn</a>
-                        <a className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200" href="http://t.me/divyaprakash_1" target="_blank">Telegram</a>
-                        <a className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200" href="mailto:rivalcoder01@gmail.com">Email</a>
-                    </div>
+                    <div className="flex space-x-4">
+                            <a
+                                className="hover:scale-110 text-white hover:text-gray-400 transition-all duration-200"
+                                href="https://github.com/Rivalcoder"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaGithub size={24} />
+                            </a>
+                            <a
+                                className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200"
+                                href="https://www.instagram.com/rival_coder?utm_source=qr&igshid=YnQ3dWxwdXFvMWNi"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaInstagram size={24} />
+                            </a>
+                            <a
+                                className="hover:scale-110 text-white hover:text-blue-600 transition-all duration-200"
+                                href="https://www.linkedin.com/in/divyaprakash-r-349746292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaLinkedin size={24} />
+                            </a>
+                            <a
+                                className="hover:scale-110 text-white hover:text-gray-600 transition-all duration-200"
+                                href="http://t.me/divyaprakash_1"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FaTelegramPlane size={24} />
+                            </a>
+                            <a
+                                className="hover:scale-110 text-white hover:text-red-600 transition-all duration-200"
+                                href="mailto:rivalcoder01@gmail.com"
+                            >
+                                <MdEmail size={24} />
+                            </a>
+                            </div>
                     <div className="flex flex-col gap-3 justify-center items-center text-base">
                         <p>Rivalcoder owned by Divyaprakash</p>
-                        <p>Copyright &copy; 2024 by @rivalcoder.vercel.app | All Rights Reserved.</p>
+                        <p>Copyright &copy; 2024 by @rivalcoder.vercel.app </p>
+                        <p> All Rights Reserved.</p>
                     </div>
                 </footer>
             </section>
